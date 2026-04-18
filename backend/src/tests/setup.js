4053eaaa34c3d8b1451b09afe423f873a088b889
Denge <tkdenge3@gmail.com>
2026-04-18 19:14:09 +0200

    fix: CI .env

--- backend/src/tests/setup.js
+++ backend/src/tests/setup.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 beforeAll(async () => {
+   if (!process.env.MONGO_URI_TEST) {
+    throw new Error("MONGO_URI_TEST is missing");
+  }
+  
   await mongoose.connect(process.env.MONGO_URI_TEST);
 
   // // clear DB at start of test run
